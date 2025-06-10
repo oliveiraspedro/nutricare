02013304cbb2ba82c8d768abb3df2f5125536331
oliveiraspedro <pedro.santosdeoliveira13@gmail.com>
2025-06-10 11:55:08 -0300

    att

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -5,13 +5,8 @@ import logo from "../../assets/img/NutriCare_Logo.webp";
 
 const Header: React.FC = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [userType, setUserType] = useState<string | null>(null);
   const [menuOpen, setMenuOpen] = useState(false);
-<<<<<<< HEAD
   const [userType, setUserType] = useState<string | null>(null);
-=======
-  
->>>>>>> a49405679f241a24b0cc6f9141ec1f54146a3a90
   const navigate = useNavigate();
   const location = useLocation();
 
@@ -19,18 +14,10 @@ const Header: React.FC = () => {
 
   useEffect(() => {
     const token = localStorage.getItem("token");
-<<<<<<< HEAD
     const userType = localStorage.getItem("userType");
     if (token) {
       setIsLoggedIn(true);
       setUserType(userType);
-=======
-    const type = localStorage.getItem("userType");
-
-    if (token && type) {
-      setIsLoggedIn(true);
-      setUserType(type);
->>>>>>> a49405679f241a24b0cc6f9141ec1f54146a3a90
     } else {
       setIsLoggedIn(false);
       setUserType(null);
@@ -42,10 +29,6 @@ const Header: React.FC = () => {
     localStorage.removeItem("userType");
     localStorage.removeItem("email");
     localStorage.removeItem("crm");
-<<<<<<< HEAD
-=======
-    
->>>>>>> a49405679f241a24b0cc6f9141ec1f54146a3a90
     setIsLoggedIn(false);
     setUserType(null);
     setMenuOpen(false);
@@ -57,12 +40,7 @@ const Header: React.FC = () => {
   return (
     <header className="header-container">
       <div className="logo-container">
-<<<<<<< HEAD
         <NavLink to="/" end onClick={() => setMenuOpen(false)}>
-=======
-        {/* O link da logo vai para a home correta se estiver logado */}
-        <NavLink to={isLoggedIn ? (userType === 'medico' ? '/pacientes' : '/dietas') : '/'} end onClick={closeMenu}>
->>>>>>> a49405679f241a24b0cc6f9141ec1f54146a3a90
           <img
             src={logo}
             alt="Logo NutriCare"
@@ -82,7 +60,6 @@ const Header: React.FC = () => {
         <div className={`bar ${menuOpen ? "open" : ""}`}></div>
         <div className={`bar ${menuOpen ? "open" : ""}`}></div>
       </div>
-<<<<<<< HEAD
 
       {!isLoggedIn ? (
         <nav className={`nav-links ${menuOpen ? "active" : ""}`}>
@@ -138,38 +115,6 @@ const Header: React.FC = () => {
           </NavLink>
         </nav>
       )}
-=======
-      
-      {/* ESTRUTURA DE NAVEGAÇÃO */}
-      <nav className={`nav-links ${menuOpen ? "active" : ""}`}>
-        {isLoggedIn ? (
-          // Links para usuários LOGADOS
-          <>
-            {userType === 'medico' && (
-              // Links específicos para MÉDICO
-              <>
-                <NavLink to="/pacientes" onClick={closeMenu} className={({ isActive }) => (isActive ? "active-link" : "")}>Home</NavLink>
-                <NavLink to="/perfil" onClick={closeMenu} className={({ isActive }) => (isActive ? "active-link" : "")}>Perfil</NavLink>
-              </>
-            )}
-            {userType === 'paciente' && (
-              // Links específicos para PACIENTE
-              <>
-                <NavLink to="/dietas" onClick={closeMenu} className={({ isActive }) => (isActive ? "active-link" : "")}>Minha Dieta</NavLink>
-                <NavLink to="/perfil" onClick={closeMenu} className={({ isActive }) => (isActive ? "active-link" : "")}>Perfil</NavLink>
-              </>
-            )}
-          </>
-        ) : (
-          // Links para visitantes (NÃO LOGADOS)
-          <>
-            <NavLink to="/" end onClick={closeMenu} className={({ isActive }) => (isActive ? "active-link" : "")}>Home</NavLink>
-            <NavLink to="/dietas" onClick={closeMenu} className={({ isActive }) => (isActive ? "active-link" : "")}>Dietas</NavLink>
-            <NavLink to="/register" onClick={closeMenu} className={({ isActive }) => (isActive ? "active-link" : "")}>Cadastro</NavLink>
-          </>
-        )}
-      </nav>
->>>>>>> a49405679f241a24b0cc6f9141ec1f54146a3a90
 
       <div className={`auth-buttons ${menuOpen ? "active" : ""}`}>
         {isLoggedIn ? (
